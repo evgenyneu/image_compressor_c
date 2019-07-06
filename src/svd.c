59c6f119b37b636f1b70ca1901c3c880149b8848
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-07-06 13:29:13 +1000

    SVD

--- src/svd.c
+++ src/svd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "svd.h"
+#include "power_method.h"
 
 Matrix *find_u_from_v(Matrix *matrix, Matrix *v, double singular_value)
 {
@@ -16,26 +17,53 @@ Matrix *find_u_from_v(Matrix *matrix, Matrix *v, double singular_value)
 SVD *svd(Matrix *matrix, int max_eigenvalues, int iterations)
 {
     SVD *result = malloc(sizeof(SVD));
+    Matrix **u_vectors = malloc((unsigned long)max_eigenvalues * sizeof(Matrix *));
+    double *singular_values = malloc((unsigned long)max_eigenvalues * sizeof(double));
+    Matrix **v_vectors = malloc((unsigned long)max_eigenvalues * sizeof(Matrix *));
 
-    printf("size of SVF %lu", sizeof(SVD));
-
-    if (result == NULL)
+    if (result == NULL || u_vectors == NULL || singular_values == NULL || v_vectors == NULL)
     {
         perror("Error allocating memory for SVD");
         exit(EXIT_FAILURE);
     }
 
-
     int iteration;
 
     for (iteration = 0; iteration < max_eigenvalues; iteration++)
     {
+        Matrix *matrix_gramian = gramian(matrix);
 
+        double eigenvalue;
+        Matrix *v = dominant_eigen_system(matrix_gramian, iterations, &eigenvalue);
+
+        if (eigenvalue == 0)
+        {
+            break;
+        }
+
+        // singular_value = math.sqrt(eigenvalue)
+        // u = find_u_from_v(matrix, v=v, singular_value=singular_value)
+        // svd_items.append((u, singular_value, v))
+
+        // if iteration == (max_eigenvalues - 1):
+        //     break
+
+        // # Calculate the first dominant term of the singular value expansion
+        // dominant = u @ np.transpose(v) * singular_value
+
+        // # Subtract the dominant term
+        // matrix = matrix - dominant
     }
 
-    // for iteration in range(max_eigenvalues):
-    //     matrix_gramian = gramian(matrix)
-    //     eigenvalue, v = dominant_eigen_system(matrix_gramian, iterations=iterations)
+    // Free memeory
+    free(u_vectors);
+    u_vectors = NULL;
+
+    free(singular_values);
+    singular_values = NULL;
+
+    free(v_vectors);
+    v_vectors = NULL;
 
     return result;
 }
